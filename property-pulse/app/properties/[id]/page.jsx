1e00794313c4c9bbba9d31f8860c6f2849639b00
RahulPrasad13400 <rahulprasad1399@gmail.com>
2025-03-30 16:19:11 +0530

    implementing profile section

--- property-pulse/app/properties/[id]/page.jsx
+++ property-pulse/app/properties/[id]/page.jsx
@@ -4,6 +4,7 @@ import PropertyHeaderImage from "@/components/PropertyHeaderImage"
 import PropertyDetails from "@/components/PropertyDetails"
 import Link from "next/link"
 import { FaArrowLeft } from "react-icons/fa"
+import PropertyImages from "@/components/PropertyImages"
 
 export default async function PropertyPage({params}) {
   await connectDB()
@@ -30,6 +31,7 @@ export default async function PropertyPage({params}) {
           </div>
         </div>
       </section>
+      <PropertyImages images={property.images} />
     </>
   )
 }
